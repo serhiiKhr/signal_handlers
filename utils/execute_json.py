2f9d3aff724685714d6c92ae0c9d366be20222d0
Serhii Khr <khrystych.serhii.work@gmail.com>
2025-04-30 16:05:17 +0300

    stft analyz completed; renderer graph in progress

--- utils/execute_json.py
+++ utils/execute_json.py
@@ -1,5 +1,6 @@
 import numpy 
 import traceback
+import uuid
 
 # readers
 from readers import MeraReader, BaseReader
@@ -43,7 +44,6 @@ settings = {
             ]
         }
     ]
-   
 }
 
 class CachedData:
@@ -64,16 +64,25 @@ class CachedData:
 class JSONExecutor:
     def __init__(self, settings):
         self.settings = settings
+        self.assign_ids(self.settings)
+        
         self.cached_data = {}
         self.processed_files = {}
-        
         self.logs = []
         
     def log_info(self, message): 
-        self.logs.append({'status': 'INFO', 'message': message})
+        if isinstance(message, list):
+            for msg in message:
+                self.logs.append({'status': 'INFO', 'message': msg})
+        else:
+            self.logs.append({'status': 'INFO', 'message': message})
         
     def log_error(self, message): 
-        self.logs.append({'status': 'ERROR', 'message': message})
+        if isinstance(message, list):
+            for msg in message:
+                self.logs.append({'status': 'ERROR', 'message': msg})
+        else:
+            self.logs.append({'status': 'ERROR', 'message': message})
         
     def show_errors(self):
         for log in self.logs:
@@ -92,104 +101,142 @@ class JSONExecutor:
         return reader.slice_signal(data=signal, sampling_rate=sampling_rate, start_time=start_time, end_time=end_time)
         
     def read_and_cache(self, file_path: str, channel: str, source_program: str):
-        key = CachedData.get_key(file_path=file_path, channel=channel)
-        if key not in self.cached_data:
-            if source_program == MERA.id:
-                data, sampling_rate = self.read_mera_file(file_path=file_path, channel=channel)
+        try:
+            key = CachedData.get_key(file_path=file_path, channel=channel)
+            if key not in self.cached_data:
+                if source_program == MERA.id:
+                    data, sampling_rate = self.read_mera_file(file_path=file_path, channel=channel)
+                else:
+                    self.log_error(message=f"Неизвестная программа источника: {source_program}")
+                    return None
+                
+                self.cached_data[key] = CachedData(
+                    file_path=file_path, 
+                    channel=channel, 
+                    source_program=source_program,
+                    data=data,
+                    sampling_rate=sampling_rate
+                )
+                print('self.cached_data[key]', self.cached_data[key])
             else:
-                self.log_error(message=f"Неизвестная программа источника: {source_program}")
-                return None
-            
-            self.cached_data[key] = CachedData(
-                file_path=file_path, 
-                channel=channel, 
-                source_program=source_program,
-                data=data,
-                sampling_rate=sampling_rate
-            )
-        else:
-            self.log_info(message=f"Файл {file_path}, канал {channel} взят из кеша")
-            
-        return self.cached_data[key]
+                self.log_info(message=f"Файл {file_path}, канал {channel} взят из кеша")
+                
+            return self.cached_data[key]
+        except Exception as e:
+            print('e ==>', e)
+            traceback.format_exc()
 
     def run(self):
+        errors = self.validate_settings(self.settings)
+        if len(errors) > 0:
+            self.log_error(errors)
+            return
+            
         try:
             for file_settings in self.settings.get('files', []):
-                file_path = file_settings['file_path']
-                time_frames = file_settings.get('time_frames', [])
-                print('time_frames ==>', time_frames)
+                id = file_settings['id']
+                settings = self.get_settings_by_id(settings=self.settings, target_id=id)
+                if not settings:
+                    return
                 
-                # Получаем настройки: локальные или глобальные
-                method_settings = file_settings.get('method', self.settings.get('method'))
+                file_path = settings["file_path"]
+                time_frames = settings["time_frames"]
+                channels = settings["channels"]
+                method = settings["method"]
+                source_program = settings["source_program"]
                 
-                channels = file_settings.get('channels', self.settings.get('channels'))
-                output_path = file_settings.get('output_path', self.settings.get('output_path'))
-                
-                source_program = file_settings.get('source_program', self.settings.get('source_program'))
+                output_path = settings["output_path"]
+                channel_groups = settings["channel_groups"]        
                 
                 if not source_program:
                     self.log_error(message=f"source_program обязательны. Ошибка в файле: {file_settings}")
-                    raise  ValueError("source_program обязателен")
+                    raise ValueError("source_program обязателен")
                 
                 # Проверка обязательных параметров
                 if not file_path:
                     self.log_error(message=f"file_path обязательны. Ошибка в файле: {file_settings}")
                     raise  ValueError("file_path обязательны")
                 
-                for channel in channels:
-                    result = self.read_and_cache(file_path=file_path, channel=channel, source_program=source_program)
-                    if result is None:
-                        continue
+                try:
+                    if len(time_frames) > 0:
+                        for tf in time_frames:
+                            self.process_file(
+                                source_program=source_program,
+                                id=id,
+                                file_path=file_path,
+                                channels=channels,
+                                time_frame=tf,
+                                method=method
+                            )
+                    else:
+                        self.process_file(
+                            source_program=source_program,
+                            id=id,
+                            file_path=file_path,
+                            channels=channels,
+                            method=method
+                        )
+                except Exception as e:
+                    print('e -->', e)
+                    traceback.format_exc()
                     
-                    self.process_file(
-                        file_path=file_path,
-                        data=result.data,
-                        channel=result.channel,
-                        sampling_rate=result.sampling_rate,
-                        time_frames=time_frames,
-                        method_settings=method_settings
-                    )  
-            
+        
             result =  self.get_results()
-            print('result ==>', result)            
+            print('result ==>', result)         
         except Exception as e:
-            self.show_errors()
-                
-
+            self.show_errors()  
 
     def process_file(
         self,
         *,
+        source_program,
+        id,
         file_path,
-        data,
-        channel,
-        sampling_rate,
-        time_frames,
-        method_settings
+        channels,
+        time_frame=None,
+        method
     ):
-        method_name = method_settings['name']
-        nperseg = method_settings.get('nperseg')
-        window = method_settings.get('window', WINDOWS['HANNING'])
-        min_freq = method_settings.get('min_freq', FREQ_FRAMES['MIN'])
-        max_freq = method_settings.get('max_freq', FREQ_FRAMES['MAX'])
+        method_name = method['name']
         
         if method_name not in self.processed_files:
             self.processed_files[method_name] = {}
-        
-        key = CachedData.get_key(file_path=file_path, channel=channel)
-        if method_name == 'stft':
-            stft = STFT(window=window, nperseg=nperseg)
-        
-            if time_frames and len(time_frames) > 0:
-                for time_frame in time_frames:
+            
+        for channel in channels:
+            cached = self.read_and_cache(file_path=file_path, channel=channel, source_program=source_program)
+            if cached is None:
+                continue
+            data = cached.data
+            sampling_rate  = cached.sampling_rate 
+    
+            key = f"{id}___{channel}"
+            if method_name == 'stft':
+                nperseg = method.get('nperseg')
+                window = method.get('window', WINDOWS['HANNING'])
+                min_freq = method.get('min_freq', FREQ_FRAMES['MIN'])
+                max_freq = method.get('max_freq', FREQ_FRAMES['MAX'])
+                
+                stft = STFT(window=window, nperseg=nperseg)
+                if time_frame:
                     sliced_data = self.slice_signal(signal=data, sampling_rate=sampling_rate, start_time=time_frame['start_time'], end_time=time_frame['end_time']) 
                     result = stft.analyze(signal=sliced_data, sampling_rate=sampling_rate, min_freq=min_freq, max_freq=max_freq)
-                    framed_key = f"{key}_{time_frame['start_time']}_{time_frame['end_time']}"
-                    self.processed_files[method_name][framed_key] = self.stft_results_handle(stft=stft, file_path=file_path, channel=channel, time_frame=time_frame, freq_frame=(min_freq, max_freq), result=result)
-            else:
-                result = stft.analyze(signal=data, sampling_rate=sampling_rate, min_freq=min_freq, max_freq=max_freq)
-                self.processed_files[method_name][key] = self.stft_results_handle(stft=stft, file_path=file_path, channel=channel, time_frame=None, freq_frame=(min_freq, max_freq), result=result)
-                        
+                    framed_key = f"{key}___{time_frame['id']}"
+                    renderer, summary = self.stft_results_handle(stft=stft, file_path=file_path, channel=channel, time_frame=time_frame, freq_frame=(min_freq, max_freq), result=result)
+                    self.processed_files[method_name][framed_key] = {
+                        'renderer': renderer,
+                        'summary': summary,
+                        'channel': channel,
+                        'file_id': id,
+                        'tf_id': time_frame['id']
+                    }
+                else:
+                    result = stft.analyze(signal=data, sampling_rate=sampling_rate, min_freq=min_freq, max_freq=max_freq)
+                    renderer, summary = self.stft_results_handle(stft=stft, file_path=file_path, channel=channel, time_frame=None, freq_frame=(min_freq, max_freq), result=result)
+                    self.processed_files[method_name][key] = {
+                        'renderer': renderer,
+                        'summary': summary,
+                        'channel': channel,
+                        'file_id': id
+                    }
         else:
             self.log_error(message=f"Метод обработки {method_name} не реализован.")
         
@@ -225,13 +272,101 @@ class JSONExecutor:
         renderer.set_ylim((0, max_y * 1.3))
         summary = f"{max_y:.2f} {y_units if y_units else ''}, {max_x:.2f} Гц"
         
-        return renderer, channel, file_path, summary
-        
+        return renderer, summary
         
     def render_graphs(self, results, settings):
         # цикл по настройкам
         # рендер результатов
+        return ''
         
+    def validate_settings(self, settings):
+        errors = []
+
+        def log(msg):
+            errors.append(msg)
+
+        global_channels = settings.get('channels')
+        global_source_program = settings.get('source_program')
+        global_method = settings.get('method')
+
+        if not isinstance(global_method, dict) or 'name' not in global_method:
+            log("Глобальные настройки 'method' должны содержать параметр 'name'.")
+
+        files = settings.get('files')
+        if not isinstance(files, list):
+            log("'files' должен быть списком.")
+            return errors
+
+        for i, file in enumerate(files):
+            file_path = file.get('file_path')
+            if not file_path:
+                log(f"[Файл {i}] Отсутствует обязательный параметр 'file_path'.")
+
+            method = file.get('method', global_method)
+            if not method or not isinstance(method, dict) or 'name' not in method:
+                log(f"[Файл {i}] Настройки 'method' отсутствуют или не содержат обязательный параметр 'name'.")
+
+            channels = file.get('channels', global_channels)
+            if not channels:
+                log(f"[Файл {i}] Не указаны каналы: ни в файле, ни глобально.")
+
+            source_program = file.get('source_program', global_source_program)
+            if not source_program:
+                log(f"[Файл {i}] Не указан 'source_program': ни в файле, ни глобально.")
+
+            time_frames = file.get('time_frames', [])
+            if not isinstance(time_frames, list):
+                log(f"[Файл {i}] 'time_frames' должен быть списком.")
+            else:
+                for j, frame in enumerate(time_frames):
+                    if not isinstance(frame, dict):
+                        log(f"[Файл {i}] time_frames[{j}] должен быть словарём.")
+                        continue
+                    for field in ['name', 'start_time', 'end_time']:
+                        if field not in frame:
+                            log(f"[Файл {i}] time_frames[{j}] отсутствует обязательное поле '{field}'.")
+
+        return errors
+    
+    def get_settings_by_id(self, settings: dict, target_id: str) -> dict:
+    
+        global_settings = {
+            "output_path": settings.get("output_path"),
+            "channels": settings.get("channels"),
+            "channel_groups": settings.get("channel_groups"),
+            "method": settings.get("method"),
+            "source_program": settings.get("source_program")
+        }        
+
+        for file_settings in settings.get("files", []):
+            file_id = file_settings.get("id")
+            file_path = file_settings.get("file_path")
+            time_frames = file_settings.get("time_frames", [])
+            # Проверка ID самого файла
+            if file_id == target_id:
+                return {
+                    "file_path": file_path,
+                    "time_frames": time_frames,
+                    "output_path": file_settings.get("output_path", global_settings["output_path"]),
+                    "channels": file_settings.get("channels", global_settings["channels"]),
+                    "channel_groups": file_settings.get("channel_groups", global_settings["channel_groups"]),
+                    "method": file_settings.get("method", global_settings["method"]),
+                    "source_program": file_settings.get("source_program", global_settings["source_program"]),
+                }
+
+        self.log_error(f"ID '{target_id}' не найден в файлах или time_frames")
+        return None     
         
+    def assign_ids(self, settings: dict) -> None:
+        for file_settings in settings.get("files", []):
+            # Добавить id файлу, если его нет
+            if "id" not in file_settings:
+                file_settings["id"] = str(uuid.uuid4())
+
+            # Добавить id каждому time_frame, если они есть
+            time_frames = file_settings.get("time_frames", [])
+            for tf in time_frames:
+                if "id" not in tf:
+                    tf["id"] = str(uuid.uuid4())
 
 
